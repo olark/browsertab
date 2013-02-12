2f0fe8a8c4c2659bdfeedd3852e9beaba0636698
Matt Pizzimenti <mjpizz+github@gmail.com>
2013-02-11 18:36:06 -0800

    built first version of BrowserTab

---
+++ README.md
@@ -0,0 +1,73 @@
+# BrowserTab
+
+Building Javascript applications that run across multiple browser tabs
+can be tricky:
+
+1. **Which browser tab is visible (if any)?** You may want to do less work
+   if you know that your current tab is not visible anyway.
+2. **Which browser tab is primary?**  You may not want all tabs to process
+   a server side event at the same time.  For example, you would not want
+   all of your tabs to generate a "new message" alert or sound.
+
+**BrowserTab** encapsulates all of this behavior into a simple API:
+
+* `BrowserTab.hidden()` true when the current tab is hidden from the visitor
+* `BrowserTab.primary()` true when the current tab is the most-recently-used tab
+
+To accomplish this, BrowserTab uses the
+[Page Visibility API](http://www.nczonline.net/blog/2011/08/09/introduction-to-the-page-visibility-api/)
+(falling back to `onblur` and `onfocus` and other heuristics) to determine
+which tab is "hidden", along with
+[Storage Events](http://html5doctor.com/storing-data-the-simple-html5-way-and-a-few-tricks-you-might-not-have-known/)
+to coordinate which tab is "primary".
+
+# Usage
+
+In the browser, just include the module with `<script src="browsertab.js"></script>`.
+Then you should be able to use the global BrowserTab object:
+
+```javascript
+if (BrowserTab.hidden()) {
+  // This tab is not visible, maybe you can stop requesting data from the server
+}
+
+if (BrowserTab.primary()) {
+  // This tab is primary, should be okay to trigger things like sounds from this one
+}
+```
+
+You can also listen for changes to hidden/primary state:
+
+```javascript
+BrowserTab.on("change:hidden", function() {
+  // This tab changed from visible to hidden, or vice-versa.
+});
+
+BrowserTab.on("change:primary", function() {
+  // This tab changed from primary to non-primary, or vice-versa.
+});
+```
+
+## Advanced Usage
+
+You can use BrowserTab in node:
+
+1. Install the module using `npm install browsertab`
+2. At the top of your code, `var BrowserTab = require("browsertab")`
+3. Create a new BrowserTab object, for example: `var tab = new BrowserTab({window: jsdom().createWindow()})`
+
+In general, you can inject all of the BrowserTab dependencies using the options:
+
+```javascript
+var tab = new BrowserTab({
+  window: window, // override window (helpful when mocking in unit tests)
+  document: document, // override document (helpful when mocking in unit tests)
+  localStorage: localStorage, // override localStorage (helpful when mocking in unit tests)
+  idStorageKey: "_BrowserTabIDStorageKey" // change the storage namespace for coordinating the "primary" tab
+})
+```
+
+## Contributing
+
+**Found a bug? Have a new idea?** Fork away and send us a pull request!
+Or [call us, maybe](https://olark.com/jobs)?
---
+++ browsertab.js
@@ -0,0 +1,286 @@
+;(function(){
+
+  /*
+  BrowserTab
+
+  Helper object for tracking whether a browser tab is "primary" and/or "hidden".
+
+  * `options.window` window object (defaults to global window)
+  * `options.document` document object (defaults to global document)
+  * `options.localStorage` localStorage to use (defaults to global localStorage)
+  * `options.idStorageKey` namespace for localStorage to coordinate "primary" tabs (defaults to "_BrowserTabIDStorageKey")
+  */
+  function BrowserTab(options) {
+    var self = this;
+    options = options || {};
+
+    this._win = options.window || window;
+    this._doc = options.document || this._win.document;
+    this._prefixes = ["webkit", "moz", "ms", "o"];
+
+    // Only allow one tab to be "primary" per domain.
+    this._localStorage = options.localStorage || this._win.localStorage;
+    this._idStorageKey = options.idStorageKey || "_BrowserTabIDStorageKey";
+    this._id = ("" + Math.random() + Math.random()).replace(/\./g, "");
+    function release() {
+      self._releasePrimary();
+    }
+    this._listen(this._win, "unload", release);
+    this._listen(this._win, "beforeunload", release);
+
+    // Use the Visibility API if available, otherwise fallback to focus/blur.
+    // http://www.nczonline.net/blog/2011/08/09/introduction-to-the-page-visibility-api/
+    if (this._supportsVisibility()) {
+      this._initializeWithVisibility();
+    } else {
+      this._initializeWithBlurring();
+    }
+
+    // Initialize primary state.
+    if (!this.hidden() || !this._anyTabsPrimary()) {
+      this._makePrimary();
+    }
+  }
+
+  ;(function(p){
+
+    // Expose class-level primary/hidden attributes for the common case.
+    var globalInstance;
+
+    BrowserTab.primary = function primary() {
+      globalInstance = globalInstance || new BrowserTab();
+      return globalInstance.primary();
+    };
+
+    BrowserTab.hidden = function hidden() {
+      globalInstance = globalInstance || new BrowserTab();
+      return globalInstance.hidden();
+    };
+
+    BrowserTab.on = function on() {
+      globalInstance = globalInstance || new BrowserTab();
+      return globalInstance.on.apply(globalInstance, arguments);
+    };
+
+    // Returns true when this is the most-recently-used tab.
+    p.primary = function() {
+      if (arguments.length > 0) {
+        throw new Error("'primary' property is not settable");
+      }
+      if (!this._localStorage) {
+        if (window.console && window.console.warn) {
+          window.console.warn("localStorage is required to use the primary() property");
+        }
+      }
+      return this._localStorage.getItem(this._idStorageKey) === this._id;
+    };
+
+    // Returns true when this tab is hidden from view.
+    p.hidden = function() {
+      if (arguments.length > 0) {
+        throw new Error("'hidden' property is not settable");
+      }
+      if (this._supportsVisibility()) {
+        return this._getProperty(this._doc, "hidden");
+      } else {
+        return this._blurred;
+      }
+    };
+
+    // Listens for changes to the hidden and primary attributes.
+    p.on = function(events, callback) {
+      events = events.split(/\s+/);
+      for (var i=0; i < events.length; i++) {
+        var event = events[i];
+        switch (event) {
+          case "change:hidden":
+            this._listenForVisibilityChange(callback);
+            break;
+          case "change:primary":
+            this._listenForPrimaryChange(callback);
+            break;
+          default:
+            throw new Error("invalid event '" + event + "'");
+        }
+      }
+    };
+
+    // Define private helpers.
+    p._getProperty = function(obj, name) {
+      var prefixableName = name.charAt(0).toString().toUpperCase() + name.slice(1);
+      if (name in obj) {
+        return obj[name];
+      } else {
+        var value;
+        this._eachBrowserPrefix(function(prefix) {
+          var prefixedName = prefix + prefixableName;
+          if (prefixedName in obj) {
+            value = obj[prefixedName];
+          }
+        });
+        return value;
+      }
+    };
+
+    p._supportsVisibility = function() {
+      var visibilityState = this._getProperty(this._doc, "visibilityState");
+      return visibilityState !== "undefined";
+    };
+
+    p._eachBrowserPrefix = function(callback) {
+      for (var i=0; i < this._prefixes.length; i++) {
+        callback(this._prefixes[i]);
+      }
+    };
+
+    p._listen = function(el, ev, fn) {
+      // http://javascriptrules.com/2009/07/22/cross-browser-event-listener-with-design-patterns/
+      // http://stackoverflow.com/questions/2490825/how-to-trigger-event-in-javascript
+      if (el.addEventListener) {
+         el.addEventListener(ev, fn, false);
+      } else if (el.attachEvent) {
+         el.attachEvent('on' + ev, fn);
+      } else {
+         el['on' + ev] = fn;
+      }
+    };
+
+    p._listenForVisibilityChange = function(callback) {
+      var self = this;
+      this._listen(this._doc, "visibilitychange", callback);
+      this._eachBrowserPrefix(function(prefix) {
+        self._listen(self._doc, prefix + "visibilitychange", callback);
+      });
+
+      // If we are using blur changes, use that callback list instead.
+      if (this._blurChangeCallbacks) {
+        this._blurChangeCallbacks.push(callback);
+      }
+    };
+
+    p._listenForPrimaryChange = function(callback) {
+      var self = this;
+      if (this._localStorage) {
+        // http://html5doctor.com/storing-data-the-simple-html5-way-and-a-few-tricks-you-might-not-have-known/
+        this._listen(this._win, "storage", function(event) {
+          event = event || window.event;
+          if (event.key === self._idStorageKey) {
+            callback();
+          }
+        });
+      }
+    };
+
+    p._makePrimary = function() {
+      if (this._localStorage) {
+        this._localStorage.setItem(this._idStorageKey, this._id);
+      }
+    };
+
+    p._releasePrimary = function() {
+      if (this._localStorage) {
+        var primaryTabID = this._localStorage.getItem(this._idStorageKey);
+        if (primaryTabID === this._id) {
+          this._localStorage.removeItem(this._idStorageKey);
+        }
+      }
+    };
+
+    p._anyTabsPrimary = function() {
+      if (this._localStorage) {
+        var primaryTabID = this._localStorage.getItem(this._idStorageKey);
+        return primaryTabID !== "undefined";
+
+      // When no localStorage available, fail open.
+      } else {
+        return false;
+      }
+    };
+
+    p._initializeWithVisibility = function() {
+      var self = this;
+      this._blurred = this.hidden();
+      this._uncertain = false;
+
+      function syncPrimary() {
+        self._blurred = self.hidden();
+        if (!self.hidden()) {
+          self._makePrimary();
+        }
+      }
+
+      syncPrimary();
+      this._listenForVisibilityChange(syncPrimary);
+    };
+
+    p._initializeWithBlurring = function() {
+      // The main limitation with this fallback is that onblur is never called
+      // when switching tabs (only when switching windows).
+      var self = this;
+      this._blurred = false;
+      this._uncertain = true;
+      this._blurChangeCallbacks = blurChangeCallbacks = [];
+
+      function triggerBlurChangeCallbacks() {
+        for (var i=0; i < blurChangeCallbacks.length; i++) {
+          setTimeout(blurChangeCallbacks[i], 0);
+        }
+      }
+
+      function blur() {
+        self._blurred = true;
+        self._uncertain = false;
+        triggerBlurChangeCallbacks();
+      }
+
+      function focus() {
+        self._blurred = false;
+        self._uncertain = false;
+        self._makePrimary();
+        triggerBlurChangeCallbacks();
+      }
+
+      // IE9 and lower have focusin/out on document.
+      this._listen(this._doc, "focusin", focus);
+      this._listen(this._doc, "focusout", blur);
+
+      // All other browsers have focus/blur on window.
+      this._listen(this._win, "focus", focus);
+      this._listen(this._win, "blur", blur);
+
+      // Since focus may not always trigger (e.g. when switching tabs),
+      // we also use other actions to consider this tab focused.
+      this._listen(this._doc, "scroll", focus);
+      this._listen(this._doc, "keydown", focus);
+      this._listen(this._doc, "mousemove", focus);
+      this._listen(this._doc, "select", focus);
+
+      this._listen(this._win, "scroll", focus);
+      this._listen(this._win, "keydown", focus);
+      this._listen(this._win, "mousemove", focus);
+      this._listen(this._win, "select", focus);
+
+      // When this becomes non-primary, it also must be blurred.
+      this._listenForPrimaryChange(function() {
+        if (!self.primary()) {
+          blur();
+        }
+      });
+    };
+
+  })(BrowserTab.prototype);
+
+  // Export for CommonJS.
+  if (this.module && this.module.exports) {
+    this.module.exports = BrowserTab;
+
+  // Export for AMD loaders.
+  } else if (this.define && this.define.amd) {
+    this.define(function(){return BrowserTab});
+
+  // For vanilla browser includes.
+  } else {
+    this.BrowserTab = BrowserTab;
+  }
+
+}).call(this);
